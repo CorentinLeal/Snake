e1ef798745034efc81a34684c6fbf83b835b837d
Dimitri Komen <komen.dimitri@gmail.com>
2015-11-25 14:11:35 +0100

    Ajout du fichier h contenant les valeurs des directions

---
+++ Snake/include/directionVariables.h
@@ -0,0 +1,10 @@
+#ifndef DIRECTIONVARIABLES_H_INCLUDED
+#define DIRECTIONVARIABLES_H_INCLUDED
+
+static const int UP = 0;
+static const int DOWN = 1;
+static const int RIGHT = 2;
+static const int LEFT = 3;
+
+
+#endif // DIRECTIONVARIABLES_H_INCLUDED
