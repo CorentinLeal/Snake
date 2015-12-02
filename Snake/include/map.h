0de975e2e17d87d3d5bb2f6edc13daf46bb68d72
CorentinLeal <leal.corentin@gmail.com>
2015-12-02 17:08:55 +0100

    ?

--- Snake/include/map.h
+++ Snake/include/map.h
@@ -1,12 +1,13 @@
 #ifndef MAP_C_INCLUDED
 #define MAP_C_INCLUDED
 
-#define MAP_WIDTH 60
-#define MAP_HEIGHT 50
+#define MAP_WIDTH 20
+#define MAP_HEIGHT 20
 
 typedef struct Map {
     int Map[MAP_HEIGHT][MAP_WIDTH];
 }Map;
 
+Map* initMap();
 
 #endif // MAP_C_INCLUDED
--- Snake/src/map.c
+++ Snake/src/map.c
@@ -2,3 +2,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "map.h"
+
+
+//Apple* initApple(){
+//    Apple* apple = malloc(sizeof(Apple));
+//    apple -> x = rand() % MAP_WIDTH;
+//    apple -> y = rand() % MAP_HEIGHT;
+//    int point = rand() % 10;
+//    if(point == 0){
+//        apple -> point = POINT_MAGIC;
+//    }else{
+//        apple -> point = POINT_CLASSIC;
+//    }
+//}
