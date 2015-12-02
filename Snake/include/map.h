3eb887acd29d8c090742f4cc9134875ae47585e2
CorentinLeal <leal.corentin@gmail.com>
2015-12-02 15:52:04 +0100

    Integration map et Snake

--- Snake/include/map.h
+++ Snake/include/map.h
@@ -1,11 +1,12 @@
 #ifndef MAP_C_INCLUDED
 #define MAP_C_INCLUDED
 
+#define MAP_WIDTH 60
+#define MAP_HEIGHT 50
+
 typedef struct Map {
     int Map[MAP_HEIGHT][MAP_WIDTH];
 }Map;
 
-static const int MAP_WIDTH = 60;
-static const int MAP_HEIGHT = 50;
 
 #endif // MAP_C_INCLUDED
--- Snake/include/mapVariables.h
+++
@@ -1,8 +0,0 @@
-#ifndef MAPVARIABLES_H_INCLUDED
-#define MAPVARIABLES_H_INCLUDED
-
-static const int MAP_WIDTH = 20;
-static const int MAP_HEIGHT = 20;
-
-
-#endif // MAPVARIABLES_H_INCLUDED
