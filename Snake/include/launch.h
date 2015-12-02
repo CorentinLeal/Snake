b1fd58511156f192f1f847398b35ac2c7616d287
Dimitri Komen <komen.dimitri@gmail.com>
2015-12-02 15:26:09 +0100

    Deportation des fonctions presentes dans le main.c vers une architecture correcte et mise en fonction de cette architecture. Le snake peut se déplacer sur un map noire.

--- Snake/include/launch.h
+++ Snake/include/launch.h
@@ -1,6 +1,18 @@
 #ifndef LAUNCH_H_INCLUDED
 #define LAUNCH_H_INCLUDED
+#include "draw.h"
+#include "game.h"
 
+typedef struct Launch Launch;
+
+
+typedef struct Launch {
+    Game* game;
+    Draw* draw;
+}Launch;
+
+Launch* initLauch();
+int play (Launch* launch);
 
 
 #endif // LAUNCH_H_INCLUDED
--- Snake/src/launch.c
+++ Snake/src/launch.c
@@ -3,6 +3,24 @@
 #include "launch.h"
 
 int main(){
-    // call game
+    Launch *launch = initLauch();
+    play(launch);
     return 0;
+} END_OF_MAIN() ;
+
+Launch* initLauch() {
+    Launch *launch = malloc(sizeof(Launch));
+    launch -> game = initGame();
+    launch -> draw = initDraw();
+    return launch;
+}
+
+int play(Launch *launch) {
+    Game* game = launch-> game;
+    Draw* draw = launch-> draw;
+    while (1) {
+        gameRound(launch -> game);
+        renderMap(game, draw);
+        rest(500);
+    }
 }
